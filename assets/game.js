0453d391db302bc7dda190eec67d540417121276
cody budge <codybudge@gmail.com>
2018-04-27 13:29:57 -0600

    All monster/hunter constructers

--- assets/game.js
+++ assets/game.js
@@ -3,7 +3,34 @@ function Monster(name,health,hits,){
     this.mnstrHealth = health;
     this.hits = hits;
 }
-var Jaggi = new MonsterStats(Jaggi,100,0);
+var monsters={
+    Jaggi: new Monster("Jaggi",100,0),
+    ablty:[]
+}
+
+function Item(name,modifier,description){
+    this.itemName = name;
+    this.itemMod = modifier;
+    this.itemDescrptn = description;
+    
+}
+var items={
+sheild: new Item("Sheild",0.3,"A strong shield!"),
+greatSword: new Item("Great Sword", 20,"A large sword made from the jaw-bone of a monster!"),
+potion: new Item("Potion", 50,"Restore your health!"),
+}
+
+function Ability(name,modifier,description){
+    this.abltyName = name;
+    this.abltyMod = modifier;
+    this.abltyDescrptn = description;
+}
+
+var abilitys={
+    bite: new Ability("Bite",15,"Monster bites you!"),
+    tailSwng: new Ability("Tail Swing",30,"Monster swings his tail."),
+    gard: new Ability("Gard",0.3,"Monster Gards!")
+}
 
 function slap() {
     health-- 
