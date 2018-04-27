2ef43f520bb2a08be9b1121efa7860f38a784f59
cody budge <codybudge@gmail.com>
2018-04-27 15:42:08 -0600

    game fix

--- assets/game.js
+++ assets/game.js
@@ -1,72 +1,92 @@
-function Monster(name,health,hits,){
-    this.mnstrName = name;
-    this.mnstrHealth = health;
-    this.hits = hits;
+// var health = 100
+// var hits=0
+function Monster(name, health) {
+    this.name = name;
+    this.health = health;
+    this.attacks = {
+        bite: 15,
+        smash: 30
+    }
+    this.items = []
+    this.hits = 0
 }
-var monsters={
-    Jaggi: new Monster("Jaggi",100,0),
-    abilitys:[]
-}
-function giveBite(){
-    abilitys.push(bite)
+//Monster Maker
+var monsters = {
+    monster: new Monster("Monster", 100),
+    frank: new Monster("Frankenstien", 100)
 }
 
 
+// var monster = {
+// health: 100,
+// attacks:{
+//     bite: 15,
+//     smash: 30,
+// },
+// items:[]
+// }
+function giveBite() {
+    monsters.frank.items.push(items.sheild)
+}
 
-function Item(name,modifier,description){
+
+function Item(name, modifier, description) {
     this.itemName = name;
     this.itemMod = modifier;
     this.itemDescrptn = description;
-    
+
 }
-var items={
-sheild: new Item("Sheild",0.3,"A strong shield!"),
-greatSword: new Item("Great Sword", 20,"A large sword made from the jaw-bone of a monster!"),
-potion: new Item("Potion", 50,"Restore your health!"),
+var items = {
+    sheild: new Item("Sheild", 0.3, "A strong shield!"),
+    greatSword: new Item("Great Sword", 20, "A large sword!"),
+    potion: new Item("Potion", 15, "Restore your health!"),
 }
 
-function Ability(name,modifier,description){
-    this.abltyName = name;
-    this.abltyMod = modifier;
-    this.abltyDescrptn = description;
+//adds all items modifiers from franks items array
+function addMods() {
+    var total = 0
+    for (var i = 0; i < monsters.frank.items.length; i++) {
+        var item = monsters.frank.items[i];
+        total+=item.itemMod
+    }
+    if (total){
+        return total
+    }
+    return 1
 }
 
-var abilitys={
-    bite: new Ability("Bite",15,"Monster bites you!"),
-    tailSwng: new Ability("Tail Swing",30,"Monster swings his tail."),
-    gard: new Ability("Gard",0.3,"Monster Gards!")
-}
 
 function slap() {
-    health-- 
+    monsters.frank.health--
     update()
     hitCnt()
 }
-function mnstrName(){
-    name
+function mnstrName() {
+    monsters.frank.name
     update()
 }
-function hitCnt(){
-    hits++
+function hitCnt() {
+    monsters.frank.hits++
     update()
 }
 
 
-function punch(){
-    health-=5
+function punch() {
+    var modifier = addMods()
+    monsters.frank.health -= 5 * modifier
     update()
     hitCnt()
 }
-function kick(){
-    health-=10
+function kick() {
+    monsters.frank.health -= 10
     update()
     hitCnt()
 }
 
 function update() {
-    document.getElementById("health").innerText = health
-    document.getElementById("hits").innerText = hits
-    document.getElementById("name").innerText = name
+    document.getElementById("health").innerText = monsters.frank.health
+    document.getElementById("hits").innerText = monsters.frank.hits
+    document.getElementById("name").innerText = monsters.frank.name
 
 }
 update()
