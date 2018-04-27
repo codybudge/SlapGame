4d77efb060255145e2c9c9805ddd2a9e57fc6bd0
cody budge <codybudge@gmail.com>
2018-04-27 12:51:19 -0600

    constrctrfnction

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,40 @@
-var health=100
-function slap(){
-if (slap){
-    health --
-    alert(health)
+function Monster(name,health,hits,){
+    this.mnstrName = name;
+    this.mnstrHealth = health;
+    this.hits = hits;
 }
+var Jaggi = new MonsterStats(Jaggi,100,0);
+
+function slap() {
+    health-- 
+    update()
+    hitCnt()
 }
-slap()
+function mnstrName(){
+    name
+    update()
+}
+function hitCnt(){
+    hits++
+    update()
+}
+
+
+function punch(){
+    health-=5
+    update()
+    hitCnt()
+}
+function kick(){
+    health-=10
+    update()
+    hitCnt()
+}
+
+function update() {
+    document.getElementById("health").innerText = health
+    document.getElementById("hits").innerText = hits
+    document.getElementById("name").innerText = name
+
+}
+update()
